7bf6a91d634af50efc1d3feeabace1fb4efef6f9
ruben.verster <ruben.verster@1-grid.com>
2021-09-29 23:45:42 +0200

    all css and html

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-Clone the repo with 'git clone REPO_HTTP_URL'
-
-Install all the package dependencies with 'npm i'
-
-Once dependencies have been installed, you can start the app by running 'npm run start', this starts the application on your local server
-
-If you're working on production, have the SCSS compiler running by using 'npm run compile-sass'
-This compiles the SCSS you write into CSS
+Care To Try Your Luck? 👀
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
 const App = () => {
-  return <h1 className="text-blue-700">Hello There</h1>;
+  return (
+    <div className="wrapper">
+      <div className="card flex flex-col justify-between items-center rounded-md">
+        <div className="card-header">
+          <h1>Flip The Coin!</h1>
+        </div>
+        <div className="card-body flex flex-col items-center">
+          <div className="coin-wrapper">
+            <div className="coin-container" onClick={() => alert('flip')}>
+              <div className="coin coin-heads">HEADS</div>
+              <div className="coin coin-tails">TAILS</div>
+            </div>
+          </div>
+        </div>
+        <div className="card-footer flex flex-col items-center">
+          <div className="guess">
+            <h2 className="guess-title">Can you predict the outcome?</h2>
+            <h2 className="guess-score">0</h2>
+            <div className="guess-options flex flex-row justify-between">
+              <div className="guess-options-option selected">Heads</div>
+              <div className="guess-options-option not-selected">Tails</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
